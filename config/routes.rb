b81f06b4b45c463855f0a155f71594032b0720a9
H3BCKN <mgonline@mgonlines-MacBook-Pro.local>
2019-07-03 00:05:29 +0200

    create separate view for each category

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  get "/categories/:id" => "categories#show", :as => "category"
   resources :pages
+
   root to: "pages#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
