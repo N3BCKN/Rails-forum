dc775e6739bad99bf04e9bd9c8f14eecb3a735ad
H3BCKN <mgonline@mgonlines-MacBook-Pro.local>
2019-07-03 20:19:04 +0200

    add user navigation bar

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,9 +1,17 @@
 module PostsHelper
-	def user_avatar(user)
+	def user_avatar(user, width, height)
 		if user.avatar.attached?
-			image_tag user.avatar, class: "img-fluid rounded-circle mt-3 ml-2"
+			image_tag user.avatar, style: "height:#{width}px;width:#{height}px;", class: "img-fluid rounded-circle mt-3 ml-2"
 		else
-			image_tag 'default_avatar.png', class: "img-fluid rounded-circle mt-3 ml-2"
+			image_tag 'default_avatar.png', style: "height:#{width}px;width:#{height}px;", class: "img-fluid rounded-circle mt-3 ml-2"
+		end
+	end
+
+	def user_avatar_tiny(user)
+		if user.avatar.attached?
+			image_tag user.avatar, style: "height:30px;width:30px;", class: "rounded-circle"
+		else
+			image_tag 'default_avatar.png', style: "height:30px;width:30px;", class: "rounded-circle"
 		end
 	end
 end
