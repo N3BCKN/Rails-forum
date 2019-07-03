a583f6a8776ad96a86169d9a7ebbe908afcd0d94
H3BCKN <mgonline@mgonlines-MacBook-Pro.local>
2019-07-03 05:52:09 +0200

    create devise user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
  
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   get "/categories/:id" => "categories#show", :as => "category"
   get "/subject/:id" => "subjects#show", :as => "subject"
   get "/discussion/:id" => "discussions#show", :as => "discussion"
