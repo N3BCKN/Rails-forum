8c20aa1a11c9929caff1210e3f6eef325cd86a84
H3BCKN <mgonline@mgonlines-MacBook-Pro.local>
2019-07-03 17:30:17 +0200

    add user avatar and helper method

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,9 @@
 module PostsHelper
+	def user_avatar(user)
+		if user.avatar.attached?
+			image_tag user.avatar, class: "img-fluid rounded-circle mt-3 ml-2"
+		else
+			image_tag 'default_avatar.png', class: "img-fluid rounded-circle mt-3 ml-2"
+		end
+	end
 end
