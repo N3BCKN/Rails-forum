1037593247c9433cb7235df2b7fd8848bd37ed3c
H3BCKN <mgonline@mgonlines-MacBook-Pro.local>
2019-07-04 04:46:33 +0200

    fix validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
 	belongs_to :discussion
 	belongs_to :user
 	paginates_per 15
+
+	validates :content, presence: true, length: { minimum: 4, maximum: 5000 }
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,4 +2,6 @@ class Subject < ApplicationRecord
 	belongs_to :category
 	has_many :discussions
 	paginates_per 20
+
+	validates :name, presence: true, length: { minimum: 5, maximum: 40 }
 end
