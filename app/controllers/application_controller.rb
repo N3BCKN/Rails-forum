9a140bfdc6039d2997e4db1db4c26b84744db9b8
H3BCKN <mgonline@mgonlines-MacBook-Pro.local>
2019-07-09 04:22:49 +0200

    minor fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # before_action :recent_discussions
+  
 
   protected
-
+  
   def configure_permitted_parameters
-    # Permit the `subscribe_newsletter` parameter along with the other
-    # sign up parameters.
     devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
   end
-
-  # def recent_discussions
-  # 	@recent_discussions = Discussion.all.order("created_at DESC").limit(5)
-  # end
 end
