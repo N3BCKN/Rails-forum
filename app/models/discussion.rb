9426d3240ec39ab0736f54928dec09e78c621df0
H3BCKN <mgonline@mgonlines-MacBook-Pro.local>
2019-07-24 22:19:44 +0200

    discussion rspec test

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -4,4 +4,7 @@ class Discussion < ApplicationRecord
   belongs_to :subject
   belongs_to :user
   has_many :posts, dependent: :destroy
+
+  validates :name, presence: true, length: { minimum: 5, maximum: 30 }
+  validates :content, presence: true, length: { minimum: 5, maximum: 5000 }
 end
--- spec/models/discussion_spec.rb
+++ spec/models/discussion_spec.rb
@@ -3,17 +3,91 @@
 require 'rails_helper'
 
 RSpec.describe Discussion, type: :model do
+  discussion = Discussion.new(
+    name: 'discussion name',
+    content: 'discussion content',
+    subject: Subject.new,
+    user: User.new
+  )
+
   describe 'validation' do
     it 'must be saved' do
+      expect(discussion.save).to eq(true)
     end
 
-    descirbe 'name' do
+    describe 'name' do
+      it 'must be present' do
+        discussion.name = nil
+        expect(discussion).to_not be_valid
+      end
+
+      it 'must be longer or equal then 5 ' do
+        discussion.name = '*' * 4
+        expect(discussion).to_not be_valid
+      end
+
+      it 'must be shorter or equal then 30' do
+        discussion.name = '*' * 31
+        expect(discussion).to_not be_valid
+      end
     end
 
-    descirbe 'content' do
+    describe 'content' do
+      it 'must be present' do
+        discussion.content = nil
+        expect(discussion).to_not be_valid
+      end
+
+      it 'must be longer or equal then 5' do
+        discussion.content = '*' * 4
+        expect(discussion).to_not be_valid
+      end
+
+      it 'must be shorter or equal then 5000' do
+        discussion.content = '*' * 5001
+        expect(discussion).to_not be_valid
+      end
+    end
+
+    describe 'user' do
+      it 'must be present' do
+        discussion = Discussion.new(
+          name: 'discussion name',
+          content: 'discussion content',
+          subject: Subject.new,
+          user: nil
+        )
+        expect(discussion).to_not be_valid
+      end
+    end
+
+    describe 'subject' do
+      it 'must be present' do
+        discussion = Discussion.new(
+          name: 'discussion name',
+          content: 'discussion content',
+          subject: nil,
+          user: User.new
+        )
+        expect(discussion).to_not be_valid
+      end
     end
   end
 
   describe 'relations' do
+    it 'should has many posts' do
+      discussion = Discussion.reflect_on_association(:posts)
+      expect(discussion.macro).to eq(:has_many)
+    end
+
+    it 'should belongs to user' do
+      discussion = Discussion.reflect_on_association(:user)
+      expect(discussion.macro).to eq(:belongs_to)
+    end
+
+    it 'should belongs to subject' do
+      discussion = Discussion.reflect_on_association(:subject)
+      expect(discussion.macro).to eq(:belongs_to)
+    end
   end
 end
